8d9b7cd0d6c6e2cfe053102f24acc461ddc5b0a9
Tyler Blovat <tyler.blovat@uconn.edu>
2020-11-29 11:47:40 -0500

    Render app now functional

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Home from "./homepage/home";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "../components/reducers/reducers";
 
 const store = createStore(reducer);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Home />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export default function Main() {
+  return (
+    <React.Fragment>
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    </React.Fragment>
+  );
+}
