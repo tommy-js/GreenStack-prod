551e507688fb44ba9cbda9b615d32a4a219195cd
Tyler Blovat <tyler.blovat@uconn.edu>
2020-12-22 22:45:06 -0500

    Updates to learn/general page

--- components/about/LearnBasicsPage/LearnBasicsPage.tsx
+++ components/about/LearnBasicsPage/LearnBasicsPage.tsx
@@ -6,7 +6,11 @@ import { MultipleChoice } from "../MultipleChoice/MultipleChoice";
 import { Blanks } from "../Blanks/Blanks";
 import { LearnGraphs } from "../LearnGraphs/LearnGraphs.jsx";
 import Router from "next/router";
-import { APPLE2MonthEffects, AMZN10YearEffects } from "../graphData.js";
+import {
+  APPLE2MonthEffects,
+  AMZN10YearEffects,
+  HTZGQ5YearEffects,
+} from "../graphData.js";
 import Link from "next/link";
 import { connect } from "react-redux";
 import { mapStateToProps } from "../../actions/actions";
@@ -66,12 +70,11 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
     options: [
       {
         text:
-          "What percent would you rate tdwa dmwkmad mwaflkwlka dkwlafllegpl fla l;w,al dmlwma his stock is in terms of buy?",
-        correctAnswer: "Yes",
+          "Answer true or false: it is better to invest in few stocks rather than many.",
+        correctAnswer: "false",
         value: 2,
         id: 0,
       },
-      { text: "Test 2", correctAnswer: "No", value: 9, id: 1 },
     ],
   };
 
@@ -86,11 +89,13 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
         <h2 className={styles.learn_page_header}>Getting Started</h2>
 
         <p className={styles.learn_page_paragraph}>
-          Getting involved in the stock market can be intimidating, especially
-          for those without a deep understanding of how the economy works.
-          Fortunately, it doesn't have to be so difficult. In this short
-          tutorial we'll break down the basics of the stock market into terms
-          that are easy to understand.
+          Getting involved in the stock market can be intimidating. From the
+          outside it seems very complicated; not only do you need to have the
+          money to invest, but you need to know how to get started with a stock
+          broker, buy and sell shares, and how to manage your risk. Fortunately,
+          it doesn't have to be so difficult. In this short tutorial we'll break
+          down the basics of the stock market into terms that are easy to
+          understand.
         </p>
         <Pie
           data={pieData}
@@ -109,9 +114,12 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
               shares
             </a>
           </Link>
-          , which fluctuate in value depending on how a company is doing. It's
-          your hope as an investor that the value of these shares increase over
-          time, so that you can sell them for a profit somewhere down the line.
+          , which fluctuate in value depending on how a company is doing.
+          Companies sell these shares in order to raise money to pursue
+          research, develop and manufacture products, pay their employees, and
+          more. It's your hope as an investor that the value of these shares
+          increase over time, so that you can sell them for a profit somewhere
+          down the line.
         </p>
         <p className={styles.learn_page_paragraph}>
           Let's take a look at some historical Apple stock data to see how this
@@ -153,12 +161,13 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
           Of course, $6.43 isn't much money. However, this brings us to one of
           the most common and successful investment strategies.
         </p>
-        <p className={styles.learn_page_subheader}>Buy, hold, buy more</p>
+        <p className={styles.learn_page_subheader}>Buy, Hold, Buy More</p>
         <p className={styles.learn_page_paragraph}>
           The goal of most investors is to make small gains slowly over time,
           and to therefore increase the value of their investment significantly
-          over a period of years. $1,000 invested into Amazon stock, AMZN, in
-          June of 2010 would have been worth $26.771.43 in October of 2020.
+          over a period of years. For example, $1,000 invested into Amazon
+          stock, AMZN, in June of 2010 would have been worth $26.771.43 in
+          October of 2020.
         </p>
         <LearnGraphs
           graphicalEffects={AMZN10YearEffects.graphicalEffects}
@@ -166,18 +175,41 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
         />
         <p className={styles.learn_page_paragraph}>
           That being said, Amazon is an extreme example of this kind of growth.
-          Invested in another popular company, Walmart, that $1,000 would be
+          Invested into another popular company, Walmart, that $1,000 would be
           worth roughly $2,769 after those ten years.
         </p>
         <p className={styles.learn_page_paragraph}>
-          Not everyone has $1,000, $10,000, or $100,000 lying around, though.
-          Many investors are starting out with very small amounts. This brings
+          Not everyone has $1,000, $10,000, or $100,000 lying around, though. A
+          substantial portion of investors are{" "}
+          <Link href="/about/glossary">
+            <a className={`${styles.emphasize} ${styles.featureless_link}`}>
+              retail investors
+            </a>
+          </Link>
+          , which are normal individuals rather than institutions. This brings
           up an important saying: "time in the market beats timing the market."
           Instead of trying to pick the perfect stock and get it at the perfect
           time, small consistent investments into the market as a whole will
-          always lead to better results.
+          almost always yield better results.
+        </p>
+        <p className={styles.learn_page_paragraph}>
+          For example, let's take the years 2010-2020 and say we invest $100 per
+          year into the NASDAQ, a stock market{" "}
+          <Link href="/about/glossary">
+            <a className={`${styles.emphasize} ${styles.featureless_link}`}>
+              index
+            </a>
+          </Link>
+          . Over this timeframe our total investment of $1000 would have grown
+          to about $2,468. While these aren't Amazon numbers, there is
+          significantly less risk involved through investing in an index than
+          there is when putting all your money into one company.
         </p>
         <p className={styles.learn_page_subheader}>Growth not guaranteed</p>
+        <LearnGraphs
+          graphicalEffects={HTZGQ5YearEffects.graphicalEffects}
+          contentsDiv="hertz_10_year"
+        />
         <p className={styles.learn_page_paragraph}>
           It's tempting to think that because certain companies have seen
           massive growth and success over a few short years, this means every
@@ -190,13 +222,31 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
           $283 in 2020.
         </p>
         <p className={styles.learn_page_paragraph}>
-          This brings up a major concept in investing; risk. Risk is the
-          likelihood that you will lose money on an investment. You can reduce
-          risk in certain ways. One of these ways is to diversify your stocks.
-          Diversifying means spreading your money out among many companies,
-          instead of just a few. Being highly diversified means that you own at
-          least 15 different shares. This means if one of those companies loses
-          value or goes bankrupt, your portfolio will take less of a hit.
+          This brings up a major concept in investing;{" "}
+          <Link href="/about/glossary">
+            <a className={`${styles.emphasize} ${styles.featureless_link}`}>
+              risk
+            </a>
+          </Link>
+          . Risk is the likelihood that you will lose money on an investment.
+          You can reduce risk in certain ways. One of these ways is to diversify
+          your stocks.{" "}
+          <Link href="/about/glossary">
+            <a className={`${styles.emphasize} ${styles.featureless_link}`}>
+              Diversification
+            </a>
+          </Link>{" "}
+          is the process of spreading your money out among many companies and
+          industries, instead of just a few. The greater the number of stocks
+          you own, and the greater the number of{" "}
+          <Link href="/about/glossary">
+            <a className={`${styles.emphasize} ${styles.featureless_link}`}>
+              industries
+            </a>
+          </Link>{" "}
+          they span, the less risk you take on. This means that if one of the
+          companies you own shares in loses value or goes bankrupt, your
+          portfolio will take less of a hit.
         </p>
 
         <Blanks
@@ -205,6 +255,35 @@ const LearnBasicsPageRender: React.FC<Redux> = (props) => {
           title={options1.title}
           options={options1.options}
         />
+        <p className={styles.learn_page_paragraph}>
+          There is good reason to invest in many different industries. In just
+          this past year we could see a very good example of why this is the
+          case. The pandemic has caused some very dramatic shifts in industries.
+          Airline stocks, for instance, have been heavily impacted by the
+          shut-downs, and have suffered as a result. Tech stocks, such as
+          Twitter and Netflix, however, have never done better.
+        </p>
+        <p className={styles.learn_page_paragraph}>
+          This isn't to say that predicting the pandemic and therefore investing
+          heavily in tech was the only way to remain solvent, of course. Well
+          diversified portfolios were no doubt able to withstand the pandemic,
+          as their losses were offset by their gains.
+        </p>
+        <p className={styles.learn_page_paragraph}>
+          Like most things concerning stocks, things aren't so simple. You
+          really need to identify your priorities when you first get started.
+          Increasing your diversification reduces your risk and the inherent{" "}
+          <Link href="/about/glossary">
+            <a className={`${styles.emphasize} ${styles.featureless_link}`}>
+              volatility
+            </a>
+          </Link>{" "}
+          you take on from the stock market. In this case, to be volatile is to
+          be subject to large jumps and drops in value. This is great for those
+          looking to slowly gain over a period of years or decades, but not
+          always so good for those looking to make a lot quicker.
+        </p>
+
         <CommentSection id="1" comments={comments} />
       </div>
       <Footer />
